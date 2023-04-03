551f2876a1e58456fef918f9aacb84a8ec8f980e
vishnu <vishnu.tvm777@gmail.com>
2023-04-03 22:15:09 +0530

    updated

---
+++ Addstudent/admin.py
@@ -0,0 +1,50 @@
+from django.contrib import admin
+from .models import Employee,student_call_status,AddStudent,State,District,Batch,Branch,Company,MapStudent,staff
+
+
+# Register your models here.
+
+
+class AddStudent_Admin(admin.ModelAdmin):
+    list_display = ('Enquiry_Source','Student' ,'Gender', 'State', 'District', 'Qualification', 'Course',)
+admin.site.register(AddStudent, AddStudent_Admin)
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=('Company','address','Contact_Person','Contact','E_mail','Website')
+    ordering = ('Company',)
+    search_fields = ('company','address','person','contact','email','website')
+admin.site.register(Employee,EmployeeAdmin)
+
+class Student_Call_Status_Admin(admin.ModelAdmin):
+    list_display = ('student_call_status','Next_FollowUp_Date','To_staff','Comment')
+    search_fields = ('student_call_status','To_staff','Next_FollowUp_Date')
+admin.site.register(student_call_status,Student_Call_Status_Admin)
+
+class State_Admin(admin.ModelAdmin):
+    list_display = ('State_Name', 'Active',)
+admin.site.register(State, State_Admin)
+
+class District_Admin(admin.ModelAdmin):
+    list_display = ('District_Name', 'Active',)
+admin.site.register(District, District_Admin)
+
+class Batch_admin(admin.ModelAdmin):
+    list_display=('Batch_Name','Start_Date','End_Date')
+admin.site.register(Batch,Batch_admin)
+
+class Branch_admin(admin.ModelAdmin):
+    list_display=('Branch_name','State','District','Mobile')
+admin.site.register(Branch,Branch_admin)
+
+class Company_admin(admin.ModelAdmin):
+    list_display=('Company','Website','Phone')
+admin.site.register(Company,Company_admin)
+
+class MapStudent_admin(admin.ModelAdmin):
+    list_display=('Student','Batch')
+admin.site.register(MapStudent,MapStudent_admin)
+
+class staff_admin(admin.ModelAdmin):
+    list_display=('staff')
+admin.site.register=(staff,staff_admin)
---
+++ Addstudent/models.py
@@ -0,0 +1,117 @@
+from django.db import models
+
+# Create your models here.
+
+
+class AddStudent(models.Model):
+    Enquiry_Source = models.CharField(max_length=10)
+    Date_of_Birth = models.DateField()
+    Student = models.CharField(max_length=20)
+    Gender = models.CharField(max_length=10)
+    Phone = models.BigIntegerField()
+    Email = models.EmailField(max_length=20)
+    Address = models.TextField(max_length=50)
+    Alternative_Email = models.CharField(max_length=20)
+    Alternative_Address = models.TextField(max_length=50)
+    Mobile = models.BigIntegerField()
+    Street = models.CharField(max_length=20)
+    City = models.CharField(max_length=20, null=True, blank=True)
+    State = models.CharField(max_length=20, null=True, blank=True)
+    District = models.CharField(max_length=20, null=True, blank=True)
+    Qualification = models.CharField(max_length=20, null=True, blank=True)
+    Course = models.CharField(max_length=20, null=True, blank=True)
+    Pincode = models.CharField(max_length=20)
+    Whatsapp = models.CharField(max_length=20)
+    RollNo = models.CharField(max_length=20)
+    Registration_No = models.CharField(max_length=20)
+    College_Name = models.CharField(max_length=20)
+    Year_of_Pass = models.IntegerField()
+    Photo = models.ImageField(upload_to="images/")
+    Active = models.BooleanField()
+
+    def __str__(self):
+        return self.Student
+
+
+
+class student_call_status(models.Model):
+    student_call_status=models.ForeignKey(AddStudent,on_delete=models.CASCADE)
+    Next_FollowUp_Date=models.DateTimeField()
+    To_staff=models.BooleanField()
+    Comment=models.CharField(max_length=200)
+    
+
+class Employee(models.Model):
+    Company=models.CharField(max_length=20)
+    address=models.CharField(max_length=20)
+    Contact_Person=models.CharField(max_length=50)
+    Contact=models.IntegerField(max_length=50)
+    E_mail=models.EmailField(max_length=50)
+    Website=models.CharField(max_length=50)
+    Activate = models.BooleanField()
+
+class State(models.Model):
+    State_Name = models.CharField(max_length=20)
+    Active = models.BooleanField()
+
+    def _str_(self):
+        return self.State_Name
+    
+class District(models.Model):
+    State_Name = models.ForeignKey(State, on_delete=models.CASCADE)
+    District_Name = models.CharField(max_length=20)
+    Active = models.BooleanField()
+
+    def _str_(self):
+        return self.District_Name
+    
+class Batch(models.Model):
+    Batch_Name = models.CharField(max_length=20)
+    Start_Date=models.DateTimeField()
+    End_Date=models.DateTimeField()
+    def __str__(self):
+        return self.Batch_Name
+
+
+    
+class Branch(models.Model):
+    Branch_name = models.CharField(max_length=20,verbose_name="Branch")
+    Address = models.CharField(max_length=50, null=True, blank=True)
+    Street = models.CharField(max_length=50, null=True, blank=True)
+    State = models.ForeignKey(State, on_delete=models.CASCADE)
+    District = models.ForeignKey(District, on_delete=models.CASCADE)
+    Pincode = models.IntegerField(max_length=10, null=True, blank=True)
+    Mobile = models.IntegerField(max_length=13)
+    E_mail = models.EmailField(max_length=20, null=True, blank=True) 
+    def _str_(self):
+        return f"{self.Branch_name} ({self.District}, {self.State})"
+
+    def get_districts(self):
+        return District.objects.filter(state=self.State)
+    
+class Company(models.Model):
+    Company = models.CharField(max_length=20)
+    Address1 =models.CharField(max_length=20, null=True, blank=True)
+    Address2 =models.CharField(max_length=20, null=True, blank=True)
+    Address3 =models.CharField(max_length=20, null=True, blank=True)
+    Phone = models.IntegerField(max_length=15)
+    E_mail = models.EmailField(max_length=20, null=True, blank=True)
+    Website = models.CharField(max_length=30)
+    Logo =models.ImageField(upload_to="images/")
+    Active = models.BooleanField()
+    def _str_(self):
+        return self.Company
+    
+class MapStudent(models.Model):
+    Student = models.ForeignKey(AddStudent,on_delete=models.CASCADE)
+    Batch = models.ForeignKey(Batch,on_delete=models.CASCADE)
+    
+
+class staff(models.Model):
+    staff = models.CharField(max_length=20)
+    def __str__(self):
+        return self.staff
+
+
+
+
